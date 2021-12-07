0e80be6e9092361c892f80e18bc0801ab8ead284
Patrik Švehla <svehlap.03@spst.eu>
2021-12-07 21:51:13 +0100

    KONEC

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -107,6 +107,7 @@ namespace POLE
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "POLE1";
             this.ResumeLayout(false);
